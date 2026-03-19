00c90c913fa799caf0371db5421fac707c47aa77
madilijosphat <madilijosphat@gmail.com>
2026-03-19 10:25:29 +0300

    UAT changes

---
+++ storage/framework/views/aafc4c92c76acb0989448f06a65423d2a83aab15.php
@@ -0,0 +1,43 @@
+<div class="page-main-header">
+  <div class="main-header-right row m-0">
+    <div class="main-header-left">
+    <!-- public/assets/images/logo/mwalimu2.png -->
+    <div class="logo-wrapper">
+    <a href="<?php echo e(route('home')); ?>">
+        <img class="img-fluid" src="<?php echo e(asset('assets/images/logo/mwalimu2.png')); ?>" alt="" style="width: 120px; height: 50px;">
+    </a>
+</div>
+      <div class="dark-logo-wrapper"><a href="<?php echo e(route('home')); ?>"><img class="img-fluid" src="<?php echo e(asset('assets/images/logo/pep.png')); ?>" alt=""></a></div>
+      <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle">    </i></div>
+    </div>
+    <div class="left-menu-header col">
+      <ul>
+        <li>
+          <div class="form-inline search-form">
+            <div class="search-bg">
+            
+          </div>
+          </div>
+          
+        </li>
+      </ul>
+    </div>
+    <div class="nav-right col pull-right right-menu p-0">
+      <ul class="nav-menus">
+        <li><a class="text-dark" href="#!" onclick="javascript:toggleFullScreen()"><i data-feather="maximize"></i></a></li>
+        
+        <li class="onhover-dropdown p-0">
+          <a class="btn btn-primary-light" href="<?php echo e(route('logout')); ?>"><i data-feather="log-out"></i>Log out</a>
+        </li>
+      </ul>
+    </div>
+    <div class="d-lg-none mobile-toggle pull-right w-auto"><i data-feather="more-horizontal"></i></div>
+  </div>
+</div>
+
+
+ 
+
+
+
+<?php /**PATH /Users/josephatwilliammadili/Desktop/UPDATED PROJECTS/EXTERNAL/MCB ESS/FRONT END/ESS-MCB-FRONTEND/resources/views/layouts/admin/partials/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c79ca7d1370f5518b937820291bd1a94df34d824.php
@@ -0,0 +1,66 @@
+
+<div class="card">
+    <div class="card-header">
+      <div class="header-top d-sm-flex align-items-center">
+        <h5>Applications Status</h5>
+      </div>
+    </div>
+    <div class="card-body p-0">
+        <figure class="highcharts-figure">
+            <div id="container_quotation_status"></div>
+        </figure>
+        
+    </div>
+  </div>
+
+<?php $__env->startPush('css'); ?>
+<style>
+    #container_quotation_status
+    {
+        width: 100%;
+    }
+</style>
+<?php $__env->stopPush(); ?>
+
+<?php $__env->startPush('scripts'); ?>
+<script>
+Highcharts.chart('container_quotation_status', 
+{
+chart: 
+{
+    type: 'pie',
+    options3d: 
+    {
+        enabled: true,
+        alpha: 45
+    }
+},
+title: 
+{
+    text: '',
+    align: 'left'
+},
+subtitle: 
+{
+    text: '',
+    align: 'left'
+},
+plotOptions: 
+{
+    pie: {
+        innerSize: 100,
+        depth: 50
+    }
+},
+series: [{
+    name: 'Customers',
+    data: <?php echo json_encode($products_performance); ?>
+
+}]
+});
+
+</script>
+<?php $__env->stopPush(); ?>
+
+
+<?php /**PATH /Users/josephatwilliammadili/Desktop/UPDATED PROJECTS/EXTERNAL/MCB ESS/FRONT END/ESS-MCB-FRONTEND/resources/views/livewire/components/reports/quotationstatus.blade.php ENDPATH**/ ?>

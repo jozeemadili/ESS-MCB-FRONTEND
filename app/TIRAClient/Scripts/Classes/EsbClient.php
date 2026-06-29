72b671d00b4c1902e758d1ff1a83b6a1da2bd3c7
madilijosphat <madilijosphat@gmail.com>
2026-06-29 17:13:27 +0300

    branch debug

--- app/TIRAClient/Scripts/Classes/EsbClient.php
+++ app/TIRAClient/Scripts/Classes/EsbClient.php
@@ -66,7 +66,7 @@ class EsbClient
                 'base_uri' => $base_uri.'/',
                 'verify' => false, // Disable SSL certificate verification
             ]);
-            dd("url ".$path_url);
+            // dd("url ".$path_url);
             $request = new Request('POST', $path_url);
             $res = $client->sendAsync($request)->wait();
             // $result= $res->getBody();
